0aaaee1862b7a4b2e240a499756b3190f2379048
Fayiza <fayiza.kt@gmail.com>
2025-08-22 00:55:29 +0400

    banner changed

--- src/components/CarousalSection/CarousalSection.tsx
+++ src/components/CarousalSection/CarousalSection.tsx
@@ -7,7 +7,7 @@ function CarousalSection() {
     <div>
       <Carousel>
       <Carousel.Item>
-        <img className="d-block w-100 h-10" src="https://m.media-amazon.com/images/I/71cJffyWqkL._SX3000_.jpg" alt="First slide" />
+        <img className="d-block w-100 h-10" src="https://m.media-amazon.com/images/I/71S1hk8tu6L._SX3000_.jpg" alt="First slide" />
       </Carousel.Item>
       <Carousel.Item>
         <img className="d-block w-100 h-10" src="https://m.media-amazon.com/images/I/71a8+gOXm+L._SX3000_.jpg" alt="Second slide" />
